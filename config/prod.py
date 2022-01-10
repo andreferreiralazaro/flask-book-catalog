f06d0bd3b1be56d88a76029b074768ff69d9b0b8
Andre Lazaro <andre.madureira@gmail.com>
2022-01-10 15:12:46 -0300

    prod.py modified to fix SQLAlchemy plugin issue

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,10 @@
 import os
+import re
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False

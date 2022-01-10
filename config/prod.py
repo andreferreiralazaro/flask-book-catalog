139e7159204f9b370f6492a7d3617249f89224f8
Andre Lazaro <andre.madureira@gmail.com>
2022-01-10 15:44:51 -0300

    fixed errors in prod.py

--- config/prod.py
+++ config/prod.py
@@ -3,8 +3,9 @@ import re
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#uri = os.getenv("DATABASE_URL")  # or other relevant config var
+#if uri.startswith("postgres://"):
+#    uri = uri.replace("postgres://", "postgresql://", 1)
+#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
 SQLALCHEMY_TRACK_MODIFICATIONS = False

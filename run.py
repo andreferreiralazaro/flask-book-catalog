73b6ef2c8a71f1984cf298fc809c54d1a71e7172
Andre Lazaro <andre.madureira@gmail.com>
2022-01-10 15:01:49 -0300

    fixed errors

--- run.py
+++ run.py
@@ -7,10 +7,9 @@ from app.auth.models import User
 flask_app = create_app('prod') # vai criar a instância da aplicação flask com a configuração 'prod' para implantação
 with flask_app.app_context():
 
-        '''como a aplicação está escalável, a instância de aplicação flask pode utilizar diversas configurações. Deste 
-        modo, o banco de dados criado vai depender do contexto da aplicação que será criado em create_app (neste caso, 
-        será criado um banco de dados da configuração relativa a 'dev' '''
-
+    '''como a aplicação está escalável, a instância de aplicação flask pode utilizar diversas configurações. Deste 
+    modo, o banco de dados criado vai depender do contexto da aplicação que será criado em create_app (neste caso, 
+    será criado um banco de dados da configuração relativa a 'dev' '''
     db.create_all()
     if not User.query.filter_by(user_name='Harry').first():
         User.create_user(user='Harry',
